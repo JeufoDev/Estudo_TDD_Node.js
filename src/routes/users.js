1d2eab9f11b45ebfa767f85c93d9bb92e653ca29
JeufoDev <llucasjefferson76@gmail.com>
2025-09-10 15:31:35 -0300

    criado camada de serviço

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,14 +1,14 @@
 module.exports = (app) => {
     const findAll = (req, res) => {
-        app.db('users').select()
+        app.services.user.findAll()
             .then(result => {
                 res.status(200).json(result);
             })
-        
+
     }
 
     const create = async (req, res) => {
-        const result = await app.db('users').insert(req.body, '*') //Esse segundo parâmetro informa o que quer retornar, o * é tudo
+        const result = await app.services.user.save(req.body) //Esse segundo parâmetro informa o que quer retornar, o * é tudo
         res.status(201).json(result[0])
     }
 
